c8a5be30717338ec977df8ca384135e4b9ce1b6b
k-ghale <kabinghale888@gmail.com>
2024-09-23 01:37:04 -0400

    add arrays.java

---
+++ arrays.java
@@ -0,0 +1,14 @@
+public class arrays {
+    //Declaring Arrays in Java
+    public static void main(String[] args) {
+        // int[] a;
+        // a = new int[5];
+        // for(int i = 0 ; i < 5 ; i++){
+        //     a[i] = 0;
+        // }
+
+        // double[] a = new double[5];
+
+        // int[] a = {1,2,3,4,5};
+    }
+}
