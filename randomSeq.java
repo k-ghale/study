572b624430661660b9d835ca3790e90e85be6077
k-ghale <kabinghale888@gmail.com>
2024-09-24 01:51:58 -0400

    random number

--- randomSeq.java
+++ randomSeq.java
@@ -1,9 +1,9 @@
-import java.util.Scanner;
+// import java.util.*;
 public class randomSeq {
     public static void main(String[] args) {
-        double sum = 0.0;
-        int cnt = 0;
-        Scanner sc = new Scanner(System.in);
-        sc.close();
+        double x = Math.random();
+        double y = Math.random();
+        System.out.println(x);
+        System.out.println(y);
     }
 }
