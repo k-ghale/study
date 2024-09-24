f84ea13fc391630b9962f638feef85ea0e1eec75
k-ghale <kabinghale888@gmail.com>
2024-09-24 01:46:57 -0400

    add randomSeq.java

---
+++ randomSeq.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class randomSeq {
+    public static void main(String[] args) {
+        double sum = 0.0;
+        int cnt = 0;
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+    }
+}
