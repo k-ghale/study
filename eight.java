f7cb976c33b9022534ea22737761f79c468fd39c
k-ghale <kabinghale888@gmail.com>
2024-09-25 22:26:45 -0400

    add eight.java

---
+++ eight.java
@@ -0,0 +1,14 @@
+
+
+public class eight {
+  String fName = "Kabin"; 
+  String lName = "Ghale";
+  int age = 21;
+
+  public static void main(String[] args) {
+    eight Obj1 = new eight();
+
+    System.out.println("Name :"+ Obj1.fName + " " + Obj1.lName);
+    System.out.println("Age:"+ Obj1.age);
+  }
+}
