814614d1f33104e73d1997ed91fd85bf4dce264d
k-ghale <kabinghale888@gmail.com>
2024-10-05 03:02:15 -0400

    updated random sequence

--- randomSeq.java
+++ randomSeq.java
@@ -3,7 +3,9 @@ public class randomSeq {
     public static void main(String[] args) {
         double x = Math.random();
         double y = Math.random();
+        System.out.println("Random Numbers are :");
         System.out.println(x);
         System.out.println(y);
+
     }
 }
